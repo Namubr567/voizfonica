c5655910f4ae247565d694b509179db1c3b61e05
Namubr567 <88306035+Namubr567@users.noreply.github.com>
2022-07-12 17:28:11 +0530

    Add files via upload

---
+++ loginbackend/src/main/java/com/example/entities/ErrorClazz.java
@@ -0,0 +1,24 @@
+package com.example.entities;
+
+public class ErrorClazz {
+	private int code;
+	private String errorMessage;
+	public ErrorClazz(int code, String errorMessage) {
+		super();
+		this.code = code;
+		this.errorMessage = errorMessage;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	}
